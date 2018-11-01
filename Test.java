0d484cfbe6ebe2975733fccc205a35240cfbe471
rat <jim.wit@188.com>
2018-11-01 22:47:54 +0000

    包例子

--- Test.java
+++ Test.java
@@ -1,10 +1,14 @@
-//字符串连接方法;
+import com.baobao.*;
 public class Test{
 	public static void main(String[] args){
 		String s="Hello,";
 		StringBuilder builder=new StringBuilder();
 		builder.append(s);
 		builder.append("Jim!");
+
+		Sm sm=new Sm();
+		builder.append(sm.get_k());
+
 		String completedString=builder.toString();
 		System.out.println(completedString);
 	}
---
+++ com/baobao/Sm.java
@@ -0,0 +1,10 @@
+package com.baobao;
+public class Sm{
+	String k="baobao";
+	void Sm(){
+	}
+	public String get_k(){
+		return k;
+	}
+
+}
